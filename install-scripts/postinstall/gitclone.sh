0ffcd270bd5a99b4d149e196613a57d2463f8a15
Shoshta73 <128172135+Shoshta73@users.noreply.github.com>
2023-08-19 11:58:04 +0200

    rename scripts to install scripts

---
+++ install-scripts/postinstall/gitclone.sh
@@ -0,0 +1,17 @@
+fontflex_clone(){
+  cd ~/Coding/FontFlex/
+  git clone https://github.com/FontFlex/Gist
+  git clone https://github.com/FontFlex/FontFlex
+  git clone https://github.com/FontFlex/fontflex.github.io
+  git clone https://github.com/FontFlex/FontFlex-cloudflare
+  git clone https://github.com/FontFlex/GhPagesJayceOldCode
+  git clone https://github.com/FontFlex/FontFlex-Dev
+  git clone https://github.com/FontFlex/DevNotes
+  git clone https://github.com/FontFlex/FlexNote-dev
+  git clone https://github.com/FontFlex/.github
+  git clone https://github.com/FontFlex/.github-private
+  git clone https://gitea.com/FontFlex/FlexNote
+  cd
+}
+
+# personal_clone(){}
